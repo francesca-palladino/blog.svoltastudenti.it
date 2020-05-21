3d03a320d0b81a7eeb0b9d4549c1ed43526e6a27
Fabio Mauri <cripty2001@users.noreply.github.com>
2020-05-21 23:22:21 +0200

    Create ipdrivers.sh

---
+++ ipdrivers.sh
@@ -0,0 +1,4 @@
+cd /etc/pki/rpm-gpg/
+wget http://download.paravelsystems.com/CentOS/7com/RPM-GPG-KEY-Paravel-Broadcast
+cd /etc/yum.repos.d/
+wget http://download.paravelsystems.com/CentOS/7com/Paravel-Commercial.repo
