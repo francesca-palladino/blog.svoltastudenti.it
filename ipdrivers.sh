a46efd7fc7decc9e3133041420e5c8e60831d9f7
Fabio Mauri <cripty2001@users.noreply.github.com>
2020-05-21 23:23:08 +0200

    Delete ipdrivers.sh

--- ipdrivers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /etc/pki/rpm-gpg/
-wget http://download.paravelsystems.com/CentOS/7com/RPM-GPG-KEY-Paravel-Broadcast
-cd /etc/yum.repos.d/
-wget http://download.paravelsystems.com/CentOS/7com/Paravel-Commercial.repo
